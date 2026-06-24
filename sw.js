710ab375a4c46e14ba0f711ccd63c82aaf4d5843
Centralppk-tools <maksbarybin9458@gmail.com>
2026-06-24 16:17:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Версия релиза приложения — менять при каждом выкладке (сейчас 2.3.1)
-const CACHE_VERSION = 'cppk_v2_3_1';
+// Версия релиза приложения — менять при каждом выкладке (сейчас 2.3.2)
+const CACHE_VERSION = 'cppk_v2_3_2';
 const CACHE_NAME = `cppk_assistant_${CACHE_VERSION}`;
 
 const PRECACHE_ASSETS = [
