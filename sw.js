8790113d3794d6ba6d83cce0c4cfb6ef9dc54afe
Centralppk-tools <maksbarybin9458@gmail.com>
2026-08-15 14:01:55 +0300

    3.1.7
    - Обновление часов смен от 15-16.08.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Версия релиза приложения — менять при каждом выкладке (сейчас 3.1.6 STABLE)
-const CACHE_VERSION = 'da_v3_1_6';
+// Версия релиза приложения — менять при каждом выкладке (сейчас 3.1.7 STABLE)
+const CACHE_VERSION = 'da_v3_1_7';
 const NOTIFICATION_ICON = './assets/app-icon.png';
 const CACHE_NAME = `digital_assistant_${CACHE_VERSION}`;
 
