649bb38ffc3cf49fcbb0c338d3fd562f3535f6e7
Maxim <marnowhere@users.noreply.github.com>
2026-09-02 12:33:10 +0300

    deploy: v4.0.7 — GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Версия релиза приложения — менять при каждом выкладке (сейчас 4.0.6)
-const CACHE_VERSION = 'da_v4_0_6';
+// Версия релиза приложения — менять при каждом выкладке (сейчас 4.0.7)
+const CACHE_VERSION = 'da_v4_0_7';
 const NOTIFICATION_ICON = './assets/app-icon.png';
 const CACHE_NAME = `digital_assistant_${CACHE_VERSION}`;
 
